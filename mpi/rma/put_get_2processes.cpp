1cb8cf3c63f29d2664b1e55cfc9dbd91df0250bf
Jayyee-HPC <jie.yang@beboplogin1.lcrc.anl.gov>
2021-03-07 17:04:36 -0600

    rma: an simple put get example.

--- mpi/rma/put_get_2processes.cpp
+++ mpi/rma/put_get_2processes.cpp
@@ -2,13 +2,14 @@
 #include <iostream>
 #include <stdlib.h>
 
-//~/test/mpich/build/_inst/bin/mpicxx -o prog  -std=c++17 put_get_2processes.cpp
-//~/test/mpich/build/_inst/bin/mpirun -np 2 ./prog
+//~/test/op_equal/mpich/_inst/bin/mpicxx -o prog  -std=c++17 put_get_2processes.cpp
+//~/test/op_equal/mpich/_inst/bin/mpirun -np 2 ./prog
 
 int main(int argc, char ** argv)
 {
+	//MPI initial
 	MPI_Init(&argc,&argv);
-    //MPI initial
+
     int myWorldRank, targetRank;
     int numWorldNodes;
     long unsigned int *my_base = NULL;
@@ -34,7 +35,9 @@ int main(int argc, char ** argv)
 
     getBuf[0] = getBuf[1] = 0;
     /* Allocate ELEM_PER_PROC integers for each process */
-    
+    putBuf[0] = (myWorldRank+101) * 1;
+    putBuf[1] = (myWorldRank+101) * 2;
+
     MPI_Win_create(my_base, sizeof(long unsigned int) * 2, sizeof(long unsigned int), MPI_INFO_NULL, MPI_COMM_WORLD,
                    &global_win);
 
@@ -45,16 +48,18 @@ int main(int argc, char ** argv)
     MPI_Put(putBuf, 2, MPI_UNSIGNED_LONG, targetRank,
                     0, 2, MPI_UNSIGNED_LONG, global_win);
 
-        MPI_Win_flush(targetRank, global_win);
+    MPI_Win_flush(targetRank, global_win);
 
 	MPI_Get(getBuf, 2, MPI_UNSIGNED_LONG, targetRank,
                     0, 2, MPI_UNSIGNED_LONG, global_win);
-
-	std::cout<<getBuf[0]<<" "<<getBuf[1]<<std::endl;
-
+	
 	MPI_Win_flush(targetRank, global_win);
+	std::cout<<myWorldRank<<" "<<getBuf[0]<<" "<<getBuf[1]<<std::endl;
+
     MPI_Win_unlock(targetRank, global_win);
 
+
+	std::cout<<myWorldRank<<" "<<my_base[0]<<" "<<my_base[1]<<std::endl;
     putBuf[0] = putBuf[1] = myWorldRank + 10;
     //MPI_Win_lock(MPI_LOCK_EXCLUSIVE, myWorldRank, 0, global_win);
 
