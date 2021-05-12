4768800b931de815ab14c2ddfc9c5d32a4be304b
Jie Yang <jie.yang@marquette.edu>
2021-05-12 18:35:20 -0500

    introduce quadtree partition

---
+++ geoms/grider/quadtree_grider/include/global_var.h
@@ -0,0 +1,14 @@
+#ifndef __GSJ_GLOBAL_VAR_H_INCLUDE__
+#define __GSJ_GLOBAL_VAR_H_INCLUDE__
+
+/* Compiler instructions*/
+#define USE_UNSTABLE_GEOS_CPP_API
+
+/* Global Variables */
+#define VERBOSE 1
+
+/* Types */
+typedef unsigned int uint;
+typedef unsigned long ulong;
+
+#endif //ndef __GSJ_GLOBAL_VAR_H_INCLUDE__
